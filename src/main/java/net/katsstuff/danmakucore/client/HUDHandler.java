ba7aad09148d8182427293ac3592a171fdc36df9
Katrix- <katrix97@hotmail.com>
2016-11-20 00:24:04 +0100

    More work on hud stuff

--- src/main/java/net/katsstuff/danmakucore/client/HUDHandler.java
+++ src/main/java/net/katsstuff/danmakucore/client/HUDHandler.java
@@ -27,7 +27,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class HUDHandler {
 
-	private final ResourceLocation barResource = new ResourceLocation(LibMod.MODID, "textures/entity/danmaku/White.png");
+	private static final ResourceLocation WHITE = new ResourceLocation(LibMod.MODID, "textures/entity/danmaku/White.png");
+	private static final ResourceLocation LIFE_FULL = new ResourceLocation(LibMod.MODID, "textures/gui/lifeFull.png");
+	private static final ResourceLocation LIFE_EMPTY = new ResourceLocation(LibMod.MODID, "textures/gui/lifeEmpty.png");
+	private static final ResourceLocation BOMB_FULL = new ResourceLocation(LibMod.MODID, "textures/gui/bombFull.png");
+	private static final ResourceLocation BOMB_EMPTY = new ResourceLocation(LibMod.MODID, "textures/gui/bombEmpty.png");
 
 	@SubscribeEvent
 	public void onDraw(RenderGameOverlayEvent.Post event) {
@@ -43,38 +47,50 @@ public class HUDHandler {
 
 				float filled = data.getPower() / 4F;
 				int widthFull = 128;
-				int width = (int)(widthFull * filled);
-				int height = 16;
+				int widthNotUsed = 32;
+				int height = 10;
 
-				int baseX = res.getScaledWidth() - 8;
-				int baseY = res.getScaledHeight() - 26;
+				int widthUsed = widthFull - widthNotUsed;
+				int widthFilled = (int)(widthUsed * filled);
 
-				int x = baseX - width;
+				int baseX = res.getScaledWidth() + 27;
+				int baseY = res.getScaledHeight() - 24;
+
+				int x = baseX - widthFilled - widthNotUsed;
 				int y = baseY - (height / 2);
 
-
-				mc.getTextureManager().bindTexture(barResource);
+				mc.getTextureManager().bindTexture(WHITE);
 
 				GlStateManager.color(1F, 1F, 1F);
-				Gui.drawModalRectWithCustomSizedTexture(baseX - 130, y - 2, 0F, 0F, widthFull + 4, height + 4, 8, 8);
+				Gui.drawModalRectWithCustomSizedTexture(baseX - widthFull - 2, y - 2, 0F, 0F, widthUsed + 4, height + 4, 8, 8);
 
 				GlStateManager.color(0F, 0F, 0F);
-				Gui.drawModalRectWithCustomSizedTexture(baseX - 128, y, 0F, 0F, widthFull, height, 8, 8);
+				Gui.drawModalRectWithCustomSizedTexture(baseX - widthFull, y, 0F, 0F, widthUsed, height, 8, 8);
 
 				GlStateManager.color(1F, 0F, 0F);
-				Gui.drawModalRectWithCustomSizedTexture(x, y, 0F, 0F, width, height, 8, 8);
+				Gui.drawModalRectWithCustomSizedTexture(x, y, 0F, 0F, widthFilled, height, 8, 8);
 
-				for(int i = 0; i < data.getLives(); i++) {
-					Gui.drawModalRectWithCustomSizedTexture(8 + (i * 12), baseY - 13, 0F, 0F, 8, 8, 8, 8);
+				int lives = data.getLives();
+				mc.getTextureManager().bindTexture(LIFE_FULL);
+				GlStateManager.color(1F, 1F, 1F);
+				for(int i = 0; i < 9; i++) {
+					if(i == lives) {
+						mc.getTextureManager().bindTexture(LIFE_EMPTY);
+					}
+					Gui.drawModalRectWithCustomSizedTexture(8 + (i * 11), baseY - 13, 0F, 0F, 8, 8, 8, 8);
 				}
 
-				GlStateManager.color(0F, 1F, 0F);
-				for(int i = 0; i < data.getBombs(); i++) {
-					Gui.drawModalRectWithCustomSizedTexture(8 + (i * 12), baseY, 0F, 0F, 8, 8, 8, 8);
+				int bombs = data.getBombs();
+				mc.getTextureManager().bindTexture(BOMB_FULL);
+				for(int i = 0; i < 9; i++) {
+					if(i == bombs) {
+						mc.getTextureManager().bindTexture(BOMB_EMPTY);
+					}
+					Gui.drawModalRectWithCustomSizedTexture(8 + (i * 11), baseY, 0F, 0F, 8, 8, 8, 8);
 				}
 
-				mc.fontRendererObj.drawStringWithShadow("Power: " + data.getPower(), baseX - 128, baseY - 30, 0xFFFFFF);
-				mc.fontRendererObj.drawStringWithShadow("Score: " + data.getScore(), baseX - 128, baseY - 20, 0xFFFFFF);
+				mc.fontRendererObj.drawStringWithShadow("Power: " + data.getPower(), baseX - widthFull, baseY - 30, 0xFFFFFF);
+				mc.fontRendererObj.drawStringWithShadow("Score: " + data.getScore(), baseX - widthFull, baseY - 20, 0xFFFFFF);
 
 				GlStateManager.popMatrix();
 			}
