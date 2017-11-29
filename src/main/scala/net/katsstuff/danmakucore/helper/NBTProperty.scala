b8380dd4432cd4307af87ad1ed651f6a93169ffc
Unknown <katrix97@hotmail.com>
2017-11-29 10:53:56 +0100

    Add copyright headers to some missing files

--- src/main/scala/net/katsstuff/danmakucore/helper/NBTProperty.scala
+++ src/main/scala/net/katsstuff/danmakucore/helper/NBTProperty.scala
@@ -1,3 +1,11 @@
+/*
+ * This class was created by <Katrix>. It's distributed as
+ * part of the DanmakuCore Mod. Get the Source Code in github:
+ * https://github.com/Katrix-/DanmakuCore
+ *
+ * DanmakuCore is Open Source and distributed under the
+ * the DanmakuCore license: https://github.com/Katrix-/DanmakuCore/blob/master/LICENSE.md
+ */
 package net.katsstuff.danmakucore.helper
 
 import java.util.{function => jfunc}
